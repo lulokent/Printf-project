7c7f933a693ea9357ef207590e7c6556801c684f
Jedidiah-Dale <jediditanya@gmail.com>
2023-11-14 13:56:12 -0800

    task 1

--- main.h
+++ main.h
@@ -9,5 +9,12 @@
 int _printf(const char *format, ...);
 int my_putcha(char c);
 int putss(char *c);
+int print_int(va_list args);
+int print_37(void);
+int _strlen(char *str);
+int printf_string(va_list val);
+int printf_char(va_list val);
+int _strlenc(const char -str);
+
 
 #endif
